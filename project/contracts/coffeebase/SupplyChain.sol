1040edcf131cc246ed33b534c6349b2e422e5e74
Mayersteen <mayersteen@gmx.de>
2021-01-13 23:29:49 +0100

    Implemented processItem() function.

--- project/contracts/coffeebase/SupplyChain.sol
+++ project/contracts/coffeebase/SupplyChain.sol
@@ -189,22 +189,25 @@ contract SupplyChain {
 
     // Increment sku
     sku = sku + 1;
-    
+
     // Emit the appropriate event
     emit Harvested(_upc);
   }
 
   // Define a function 'processtItem' that allows a farmer to mark an item 'Processed'
-  function processItem(uint _upc) public 
+  function processItem(uint _upc)
+  public
   // Call modifier to check if upc has passed previous supply chain stage
-  
+  harvested(_upc)
   // Call modifier to verify caller of this function
-  
+  verifyCaller(items[_upc].originFarmerID)
+  // This function can only be called by a Farmer
+  onlyFarmer()
   {
     // Update the appropriate fields
-    
+    items[_upc].itemState = State.Processed;
     // Emit the appropriate event
-    
+    emit Processed(_upc);
   }
 
   // Define a function 'packItem' that allows a farmer to mark an item 'Packed'

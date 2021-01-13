014dbff16621011d1f80c1ed90a580b6b656679d
Mayersteen <mayersteen@gmx.de>
2021-01-13 13:20:30 +0100

    Update solc version to avoid truffle compile error.

--- project/truffle-config.js
+++ project/truffle-config.js
@@ -16,5 +16,21 @@ module.exports = {
         gas: 4500000,
         gasPrice: 10000000000
     },
+  },
+  mocha: {
+    // timeout: 100000
+  },
+  compilers: {
+    solc: {
+       version: "^0.4.24",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
   }
 };
